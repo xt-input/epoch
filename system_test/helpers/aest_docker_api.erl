0708cd087fdeffec3c05a42ec283e84703dd1722
Dincho Todorov <dincho@users.noreply.github.com>
2018-04-23 16:29:03 +0300

    Use DOCKER_HOST env var if set

--- system_test/helpers/aest_docker_api.erl
+++ system_test/helpers/aest_docker_api.erl
@@ -362,7 +362,8 @@ json_string(Num) when is_number(Num) -> format("~w", [Num]).
 url(Path) -> url(Path, #{}).
 
 url(Path, QS) when is_list(Path) ->
-    hackney_url:make_url(?BASE_URL, [to_binary(P) || P <- Path], maps:to_list(QS));
+    BaseUrl = os:getenv("DOCKER_HOST", ?BASE_URL),
+    hackney_url:make_url(BaseUrl, [to_binary(P) || P <- Path], maps:to_list(QS));
 url(Item, QS) ->
     url([Item], QS).
 
