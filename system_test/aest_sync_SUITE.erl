0fb903675de47da87a79a538d2e51a96537829dd
Sebastien Merle <s.merle@gmail.com>
2018-04-26 18:28:46 +0200

    Add log checking to system tests

--- system_test/aest_sync_SUITE.erl
+++ system_test/aest_sync_SUITE.erl
@@ -131,7 +131,7 @@ init_per_testcase(_TC, Config) ->
                           | Config]).
 
 end_per_testcase(_TC, Config) ->
-    aest_nodes:ct_cleanup(Config).
+    ok = aest_nodes:ct_cleanup(Config).
 
 %=== TEST CASES ================================================================
 
